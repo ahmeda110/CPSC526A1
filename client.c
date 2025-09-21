bbfbd2060674e34bbb63536dcc0028648a930d5d
Ahmed Abbas <abbas@pashmotors.com>
2025-09-20 20:25:34 -0600

    “test”

--- client.c
+++ client.c
@@ -4,8 +4,6 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define SERVER_IP "10.59.174.169"  // Replace with your host IP
-#define SERVER_PORT 21493          // Replace with your UCID-based port
 #define BUFFER_SIZE 1024
 
 void send_command(int sock, const char *command) {
@@ -21,37 +19,40 @@ void send_command(int sock, const char *command) {
     }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <server_ip> <server_port>\n", argv[0]);
+        return 1;
+    }
+
+    const char *server_ip = argv[1];
+    int server_port = atoi(argv[2]);
+
     int sock;
     struct sockaddr_in server_addr;
 
-    // Create socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("Socket creation failed");
         return 1;
     }
 
-    // Set server address
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERVER_PORT);
-    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
-        perror("Invalid address");
+    server_addr.sin_port = htons(server_port);
+    if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
+        perror("Invalid IP address");
         return 1;
     }
 
-    // Connect to server
     if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("Connection failed");
         return 1;
     }
 
-    // Send commands
     send_command(sock, "connect");
     send_command(sock, "open");
     send_command(sock, "disconnect");
 
-    // Close socket
     close(sock);
     return 0;
 }
